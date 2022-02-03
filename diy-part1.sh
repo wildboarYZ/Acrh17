a4e1b663a86867a0f6586bad0c29146da75322c3
wildboarYZ <89271797+wildboarYZ@users.noreply.github.com>
2022-02-03 21:33:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,2 +1,3 @@
 # Add a feed source
-echo 'src-git kenzok8 https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
